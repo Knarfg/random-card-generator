d42edf1c1d6eb4c6ef49b259355ec0547349f386
Knarfg <frankgonzalez033@gmail.com>
2022-04-13 01:04:51 +0000

    Codigo carta aleatoria

--- src/app.js
+++ src/app.js
@@ -7,5 +7,46 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+
+  //Definiendo las variables
+  let palo = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  let carta = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  //Carta aleatoria
+  function numAleatorio(arr) {
+    var aleatorio = ~~(Math.random() * arr.length);
+    return arr[aleatorio];
+  }
+  function mostrarCarta() {
+    const suit = numAleatorio(palo);
+    //Cambiando color de corazon y diamante
+    if (suit === "&hearts;" || suit === "&diams;") {
+      let classPinta = 0;
+      let i = 0;
+      classPinta = document.querySelectorAll(".pintas");
+      for (i = 0; i < classPinta.length; i++) {
+        classPinta[i].style.color = "red";
+      }
+    }
+    document.getElementsByClassName("pintas")[0].innerHTML = suit;
+    document.getElementsByClassName("pintas")[1].innerHTML = suit;
+    // document.getElementsByClassName("centro").innerHTML = numAleatorio(carta);
+    document.getElementById("cartas").innerHTML = numAleatorio(carta);
+  }
+
+  mostrarCarta();
 };
